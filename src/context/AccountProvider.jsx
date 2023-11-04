ed74711d40532ca98f30ccfee9351a36c7bf2db6
bannu773 <90920092+bannu773@users.noreply.github.com>
2023-11-04 21:08:02 +0530

    Update AccountProvider.jsx

--- src/context/AccountProvider.jsx
+++ src/context/AccountProvider.jsx
@@ -16,7 +16,7 @@ const AccountProvider = ({children}) => {
     const socket = useRef();
 
     useEffect(() => {
-        socket.current = io('https://whatsapp-clone-socket-ncrp.onrender.com');
+        socket.current = io('https://whatsapp-clone3.onrender.com/');
     }, [])
 
     return (
